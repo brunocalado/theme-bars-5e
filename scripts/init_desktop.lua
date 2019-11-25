caa8417ea1fd35e6c667eade99c238302e9e5e78
Bruno Calado <brunocalado@users.noreply.github.com>
2019-11-25 14:21:45 -0300

    starting point

---
+++ scripts/init_desktop.lua
@@ -0,0 +1,11 @@
+function onInit()
+	
+		DesktopManager.showDockTitleText(false);
+		DesktopManager.setStackIconSizeAndSpacing(24, 19, 1, 0);
+		-- DesktopManager.setStackIconSizeAndSpacing(49, 39, 1, 0);
+		DesktopManager.setDockIconSizeAndSpacing(100, 31, 0, -2);
+		DesktopManager.setLowerDockOffset(3, 0, 0, 0);
+		DesktopManager.setMaxDockColumns(1);
+	
+end
+
